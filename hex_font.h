f54bda866acfc1435f2a30368196804c948e48db
ok97465 <ok97465@gmail.com>
2025-06-14 21:27:45 -0400

    Render hex digits using Consolas font

--- hex_font.h
+++ hex_font.h
@@ -3,10 +3,13 @@
 #include <cstdint>
 
 #define HEX_FONT_GLYPHS 16
-#define HEX_FONT_WIDTH_BYTES 3
 #define HEX_FONT_WIDTH 24
 #define HEX_FONT_HEIGHT 24
 
-extern const uint8_t HexFont24x24[HEX_FONT_GLYPHS][HEX_FONT_HEIGHT][HEX_FONT_WIDTH_BYTES];
+// Build a 16-glyph atlas from the Consolas TrueType font. The atlas array
+// is laid out row-major with `HEX_FONT_GLYPHS` glyphs each of size
+// `HEX_FONT_WIDTH` by `HEX_FONT_HEIGHT`. Each pixel is an 8-bit alpha value.
+// Returns false if the font cannot be loaded.
+bool BuildConsolasHexAtlas(unsigned char atlas[HEX_FONT_HEIGHT][HEX_FONT_WIDTH * HEX_FONT_GLYPHS]);
 
 #endif
--- ntds2d.cpp
+++ ntds2d.cpp
@@ -738,13 +738,15 @@ void InitHexTextInstancing()
 
     gHexText.program = CreateProgram(vsSrc, fsSrc);
 
-    unsigned char atlas[HEX_FONT_HEIGHT][HEX_FONT_WIDTH*HEX_FONT_GLYPHS];
-    for(int g=0; g<HEX_FONT_GLYPHS; ++g){
-        for(int y=0; y<HEX_FONT_HEIGHT; ++y){
-            for(int x=0; x<HEX_FONT_WIDTH; ++x){
-                uint8_t byte = HexFont24x24[g][y][x/8];
-                atlas[y][g*HEX_FONT_WIDTH + x] =
-                    (byte & (0x80 >> (x%8))) ? 255 : 0;
+    unsigned char atlas[HEX_FONT_HEIGHT][HEX_FONT_WIDTH * HEX_FONT_GLYPHS];
+    if (!BuildConsolasHexAtlas(atlas)) {
+        for (int g = 0; g < HEX_FONT_GLYPHS; ++g) {
+            for (int y = 0; y < HEX_FONT_HEIGHT; ++y) {
+                for (int x = 0; x < HEX_FONT_WIDTH; ++x) {
+                    uint8_t byte = HexFont24x24[g][y][x / 8];
+                    atlas[y][g * HEX_FONT_WIDTH + x] =
+                        (byte & (0x80 >> (x % 8))) ? 255 : 0;
+                }
             }
         }
     }
